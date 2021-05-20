8ab0f7a645db361f32c727e47d1adbcdd1048389
Martin Kokeš <info@martinkokes.cz>
2021-05-20 21:13:19 +0200

    fixed the namespace
    there was a missing letter "l"

--- nettrine/app/Model/Database/Fixtures/ArticlesFixture.php
+++ nettrine/app/Model/Database/Fixtures/ArticlesFixture.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace App\Mode\Database\Fixtures;
+namespace App\Model\Database\Fixtures;
 
 use App\Model\Database\Advanced\Entity\Article;
 use App\Model\Database\Advanced\Entity\ArticleCategory;
